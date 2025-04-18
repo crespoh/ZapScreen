62ed5c89e1c580c8c9fcf6622b42ea2f3f7fc649
crespoh <crespoh@gmail.com>
2025-04-18 08:59:14 +0800

    Initial Commit

---
+++ ZapScreen/ZapScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapScreenApp.swift
+//  ZapScreen
+//
+//  Created by tongteknai on 18/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ZapScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
